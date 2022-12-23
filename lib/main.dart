f8fb103a71a0eead2751bb8bc3f36f79ddbcf2fb
vviia <auliaoctaviani77@gmail.com>
2022-12-23 20:20:16 +0700

    simple CRUD

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:crud/screens/fetch_data.dart';
+import 'package:crud/screens/insert_data.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Firebase'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Image(
+              width: 300,
+              height: 300,
+              image: NetworkImage(
+                  'https://seeklogo.com/images/F/firebase-logo-402F407EE0-seeklogo.com.png'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'My CRUD with Firebase Realtime Database',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const InsertData()));
+              },
+              color: Colors.teal,
+              textColor: Colors.white,
+              minWidth: 300,
+              height: 40,
+              child: const Text('Insert Data'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const FetchData()));
+              },
+              color: Colors.red.shade200,
+              textColor: Colors.white,
+              minWidth: 300,
+              height: 40,
+              child: const Text('Fetch Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
